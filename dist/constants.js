34c2bb98a0ba63a2099229798e80e1c4ee5cae69
Joshua Comeau <joshwcomeau@gmail.com>
2021-05-01 18:03:17 -0400

    Stop committing built code

--- dist/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.EMAIL_ATTEMPT_LIMIT = void 0;
-const EMAIL_ATTEMPT_LIMIT = 3;
-exports.EMAIL_ATTEMPT_LIMIT = EMAIL_ATTEMPT_LIMIT;
