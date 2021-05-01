3dfabd4fe9fb40c7556908c0bec599fd15cd5cb1
Joshua Comeau <joshwcomeau@gmail.com>
2021-05-01 17:58:01 -0400

    More debug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Discord from 'discord.js';
 import onboardingSetup from './onboarding';
 
 function start() {
+  console.log('\n\n\n\nSTART\n');
   const client = new Discord.Client();
 
   client.login(process.env.DISCORD_BOT_TOKEN);
