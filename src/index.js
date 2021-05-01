6ab5208d4b420d6fe4a393f7bba1686ecbe86bfe
Joshua Comeau <joshwcomeau@gmail.com>
2021-05-01 18:02:33 -0400

    I'll pull this car over right now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import Discord from 'discord.js';
 import onboardingSetup from './onboarding';
 
 function start() {
-  console.log('\n\n\n\nSTART\n');
+  console.log('\n\n\n\nSTART with DATABASE NAME\n');
+  console.log(process.env.MONGODB_URI);
+  console.log(process.env.MONGODB_DB);
+  console.log('\n\n\n\n');
   const client = new Discord.Client();
 
   client.login(process.env.DISCORD_BOT_TOKEN);
